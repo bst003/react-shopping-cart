bb68b0cb86f9ef176436057a89fffc0ca2cb93eb
bst003 <bstanner33@yahoo.com>
2023-02-16 23:24:53 -0500

    Continue notes

--- src/index.js
+++ src/index.js
@@ -13,6 +13,37 @@ Project Notes
     - Cart
 
 - Components Needed
+    - RouteSwitch
+    - Header
+    - Footer
+    - MainLayout (working title, used for general site structure as highest level route)
+    - Home (home route element)
+    - Shop (Shop route element)
+    - SingleProd (Single prod route element)
+    - Cart (cart route element)
+    - HomePageHero (deco only - used on home)
+    - SubPageHero (deco only - used on shop, single prod and cart)
+    - ProductsGrid
+    - ProductsGridItem
+    - SingleProdForm
+    - CartTable
+    - Cart Table Item
+
+
+- Site general structure
+    - Every component will be rendered as inner output of Main Layout
+
+- Cart State
+    - Will live in RouteSwitch?
+    - 
+
+- Products Thoughts
+    - Just use json datasheet?
+    - Just store images in assets folder?
+
+- To Be Determined
+    - How to set up 404 route
+
 
 */
 
