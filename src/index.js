5f5727722916c79c5cd0a08fdc9533712437bfb6
bst003 <bstanner33@yahoo.com>
2023-02-16 23:05:27 -0500

    Remove unneeded files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 /*
@@ -19,11 +17,7 @@ Project Notes
 */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+root.render(<React.StrictMode>test</React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
