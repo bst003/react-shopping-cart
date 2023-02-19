eb3b06b3d6c8bc8d538011aca40752f3340772d1
bst003 <bstanner33@yahoo.com>
2023-02-19 01:02:26 -0500

    Begin filling out productData json sheet

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Shop from "./views/Shop";
+import Cart from "./views/Cart";
 import NotFound from "./views/NotFound";
 import MainLayout from "./MainLayout";
 
@@ -11,6 +12,7 @@ const RouteSwitch = () => {
                 <Route element={<MainLayout />}>
                     <Route path="/" element={<Home />} />
                     <Route path="/shop" element={<Shop />} />
+                    <Route path="/cart" element={<Cart />} />
                     <Route path="*" element={<NotFound />} />
                 </Route>
             </Routes>
