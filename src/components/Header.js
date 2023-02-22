c0872f7c489490525eb94e86ef399af076535db7
bst003 <bstanner33@yahoo.com>
2023-02-21 23:39:34 -0500

    Begin styling Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,32 @@
 // Will need to have cart quantity passed down as a prop
+import { Link } from "react-router-dom";
+import "./Header.scss";
 
-const Header = () => {
-    return <header className="site-header">Header</header>;
+const Header = (props) => {
+    return (
+        <header className="site-header">
+            <div className="site-header__inner">
+                <div className="site-logo">HydroHomies</div>
+
+                <nav className="site-nav">
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/shop">Shop</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div className="site-cart-link">
+                    <Link>
+                        <i className="fas fa-shopping-cart"></i>
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
