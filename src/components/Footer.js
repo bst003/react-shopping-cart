74b9c2f74992653bfc7c8ca3af89fb2b7b2c557d
bst003 <bstanner33@yahoo.com>
2023-02-23 20:59:16 -0500

    Update footer link target

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,14 @@ const Footer = () => {
         <footer className="site-footer">
             <div className="site-footer__inner">
                 <p>
-                    Site created by <a href="https://github.com/bst003">bst003</a>, image credit
-                    goes to{" "}
-                    <a href="https://unsplash.com/@quokkabottle">quokkabottles on Unsplash</a>
+                    Site created by{" "}
+                    <a href="https://github.com/bst003" target="_blank">
+                        bst003
+                    </a>
+                    , image credit goes to{" "}
+                    <a href="https://unsplash.com/@quokkabottle" target="_blank">
+                        quokkabottles on Unsplash
+                    </a>
                 </p>
             </div>
         </footer>
