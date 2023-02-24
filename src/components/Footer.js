828c46899657d32a04d827dd109c55c0ee198f42
bst003 <bstanner33@yahoo.com>
2023-02-23 20:58:53 -0500

    Begin segmenting style.scss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,17 @@
+import "./Footer.scss";
+
 const Footer = () => {
-    return <footer className="site-footer">footer</footer>;
+    return (
+        <footer className="site-footer">
+            <div className="site-footer__inner">
+                <p>
+                    Site created by <a href="https://github.com/bst003">bst003</a>, image credit
+                    goes to{" "}
+                    <a href="https://unsplash.com/@quokkabottle">quokkabottles on Unsplash</a>
+                </p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
