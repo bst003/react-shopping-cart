81e477b1ec9f4c22dc457d34618f3167ed6f8890
bst003 <bstanner33@yahoo.com>
2023-02-28 23:21:56 -0500

    Add basic jsx to ProdInfoForm

--- src/components/ProdInfoForm.js
+++ src/components/ProdInfoForm.js
@@ -1,5 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+/*
+What will this comp need to do?
+    Push num of products and prod data to cart
+    Display Message when added to cart
+*/
+
 const ProdInfoForm = () => {
-    return <form className="product-info-form">Product Form</form>;
+    return (
+        <form className="product-info-form">
+            <div className="field-contain">
+                <label for="quantity">Quantity</label>
+                <input id="quantity" name="quantity" type="number" min="0" max="20" />
+            </div>
+        </form>
+    );
 };
 
 export default ProdInfoForm;
