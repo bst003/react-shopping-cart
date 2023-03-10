f2659625a042c4cae4ac48d3fbf1faa7bd9cf58f
bst003 <bstanner33@yahoo.com>
2023-03-09 23:46:11 -0500

    Run test to ensure that ProdCartForm passes correct data on submit'

--- src/components/tests/ProdCartForm.test.js
+++ src/components/tests/ProdCartForm.test.js
@@ -14,4 +14,22 @@ describe("ProdCartForm component", () => {
 
         expect(onSubmitMock).toHaveBeenCalledTimes(1);
     });
+
+    it("onSubmitMock is passed the correct data", () => {
+        const onSubmitMock = jest.fn();
+        render(<ProdCartForm addToCart={onSubmitMock} id="01" name="test prod" price="30" />);
+
+        const submitBtn = screen.getByRole("button", { type: /submit/i });
+
+        const cartItem = {
+            name: "test prod",
+            id: "01",
+            price: "30",
+            quantity: 1,
+        };
+
+        userEvent.click(submitBtn);
+
+        expect(onSubmitMock).toHaveBeenNthCalledWith(1, cartItem);
+    });
 });
