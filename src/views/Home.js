4f9b2aba0342111efcdc6d2a70e4d71c9513ee0f
bst003 <bstanner33@yahoo.com>
2023-03-10 16:54:19 -0500

    Add extra filler content to Home view

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,27 @@
 import HomeHero from "../components/HomeHero";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
         <div>
             <HomeHero />
-            <h2>Home</h2>
+            <div className="wrap extra-narrow">
+                <h2 className="center">Why Buy Our Bottles?</h2>
+                <p>
+                    We sell high quality water bottles at wholesale prices to ensure that you get
+                    the best deal. In addition, if you're able to find a water bottle we carry at a
+                    lower price elsewhere we'll match that price. Here at HydroHomies we pride
+                    ourselves on always putting the customer's needs first. Now let's get shopping
+                    for the perfect water bottle.
+                </p>
+                <div className="btns-contain center">
+                    <div className="btn">
+                        <Link className="btn__link" to={"/shop"}>
+                            Shop Now
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
