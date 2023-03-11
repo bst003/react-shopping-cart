a5a9ff694b2fc98a0ba872b9c151db962b5550f0
bst003 <bstanner33@yahoo.com>
2023-03-11 17:27:50 -0500

    Update CartTableItem to calc subtotal

--- src/components/CartTableItem.js
+++ src/components/CartTableItem.js
@@ -1,12 +1,16 @@
+import React, { useEffect, useState } from "react";
+
 const CartTableItem = (props) => {
     const { id, price, name, quantity } = props;
+
+    const [editing, setEditing] = useState(false);
 
     return (
         <tr key={id} className="cart-table-contain">
             <td>{name}</td>
             <td>{price}</td>
             <td>{quantity}</td>
-            <td>SubTotal</td>
+            <td>{price * quantity}</td>
         </tr>
     );
 };
