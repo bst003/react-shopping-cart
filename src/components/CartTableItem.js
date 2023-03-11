b3406b850a4008cd5387861bb29ede000cf3554f
bst003 <bstanner33@yahoo.com>
2023-03-11 17:49:52 -0500

    Set up button to trigger edit mode for cart item quantity

--- src/components/CartTableItem.js
+++ src/components/CartTableItem.js
@@ -5,11 +5,52 @@ const CartTableItem = (props) => {
 
     const [editing, setEditing] = useState(false);
 
+    const triggerEditQuantity = () => {
+        setEditing(true);
+    };
+
+    const renderQuantityContents = () => {
+        let renderEls;
+        if (editing) {
+            renderEls = (
+                <form className="cart-update-quantity">
+                    <div className="field-contain hori">
+                        <label htmlFor="quantity">Quantity</label>
+                        <input
+                            id="quantity"
+                            name="quantity"
+                            type="number"
+                            min="1"
+                            max="20"
+                            defaultValue={quantity}
+                        />
+                    </div>
+                    <div className="form-submit btn">
+                        <button className="btn__link" type="submit">
+                            Update
+                        </button>
+                    </div>
+                </form>
+            );
+        } else {
+            renderEls = (
+                <div>
+                    {quantity}
+                    <button onClick={triggerEditQuantity} className="edit-cart" type="button">
+                        Edit
+                    </button>
+                </div>
+            );
+        }
+
+        return renderEls;
+    };
+
     return (
         <tr key={id} className="cart-table-contain">
             <td>{name}</td>
             <td>{price}</td>
-            <td>{quantity}</td>
+            <td>{renderQuantityContents()}</td>
             <td>{price * quantity}</td>
         </tr>
     );
