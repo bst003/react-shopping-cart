c4e4c7437df6fc31e4c0b6aee5763ba31a943fb3
bst003 <bstanner33@yahoo.com>
2023-03-17 11:30:26 -0400

    Add ScrollToTop component to force page to scroll to top on view change

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 import Home from "./views/Home";
 import Shop from "./views/Shop";
 import Product from "./views/Product";
@@ -82,6 +83,7 @@ const RouteSwitch = () => {
 
     return (
         <BrowserRouter basename="/react-shopping-cart">
+            <ScrollToTop />
             <Routes>
                 <Route element={<MainLayout cartQuantity={cartQuantity} />}>
                     <Route path="/" element={<Home />} />
